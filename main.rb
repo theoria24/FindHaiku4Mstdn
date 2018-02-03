6280be99c3426db2436dbb45fe73735e016dd955
theoria <theoria23@gmail.com>
2018-02-03 19:48:12 +0900

    タグ検出のバグ修正

--- main.rb
+++ main.rb
@@ -28,7 +28,8 @@ begin
           if haiku then
             postcontent = "『#{haiku.phrases[0].join("")} #{haiku.phrases[1].join("")} #{haiku.phrases[2].join("")}』"
             p "俳句検知: #{postcontent}" if debug
-            if toot.attributes["tags"].map{|t| t["name"]}.include?("theboss_tech")
+            p "tags: #{toot.attributes["tags"]}" if debug
+            if toot.attributes["tags"].map{|t| t["name"]}.include?("theboss_tech") then
               vis = "public"
             else
               vis = "unlisted"
